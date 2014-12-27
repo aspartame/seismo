6d5c1ca41297c09242126c05df1c93d87a9c1c1b
Linus Karnland <linus.karnland@gmail.com>
2014-12-27 17:28:24 +0100

    Got rid of polymer

--- js/seismo.js
+++ js/seismo.js
@@ -26,9 +26,9 @@ var SEISMO = (function () {
 var USGSFeed = function () {
 	function getFeed(url, callback) {
 		$.ajax({
-		    url: url,
-		    jsonp: 'eqfeed_callback',
-		    dataType: 'jsonp'
+			url: url,
+			jsonp: 'eqfeed_callback',
+			dataType: 'jsonp'
 		});
 	}
 	
@@ -51,12 +51,12 @@ var SeismoMap = function () {
 	function load(onLoaded) {
 		google.maps.event.addDomListener(window, 'load', function () {
 			var mapOptions = {
-			 	mapTypeId: google.maps.MapTypeId.TERRAIN,
-		     	center: { lat: 30, lng: 0},
+				mapTypeId: google.maps.MapTypeId.TERRAIN,
+				center: { lat: 30, lng: 0},
 				zoom: 3
-		    };
+			};
   	  	
-		    _map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+			_map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 			
 			onLoaded();
 		});
@@ -73,21 +73,21 @@ var SeismoMap = function () {
 		var coords = quake.geometry.coordinates;
 		
 		var marker = new google.maps.Marker({
-		  position: new google.maps.LatLng(coords[1],coords[0]),
-		  map: _map,
-		  icon: {
-		    path: google.maps.SymbolPath.CIRCLE,
-			fillOpacity: 0.6,
-		    fillColor: getMarkerColor(magnitude, 1.0),
-		    strokeOpacity: 1.0,
-		    strokeColor: getMarkerColor(magnitude, 0.6),
-		    strokeWeight: 1.0, 
-			scale: Math.pow(magnitude, 1.8) - 0.8 * magnitude // Some random algo to scale circles
-		  }
+			position: new google.maps.LatLng(coords[1],coords[0]),
+			map: _map,
+			icon: {
+				path: google.maps.SymbolPath.CIRCLE,
+				fillOpacity: 0.6,
+				fillColor: getMarkerColor(magnitude, 1.0),
+				strokeOpacity: 1.0,
+				strokeColor: getMarkerColor(magnitude, 0.6),
+				strokeWeight: 1.0, 
+				scale: Math.pow(magnitude, 1.8) - 0.8 * magnitude // Some random algo to scale circles
+			}
 		});
 		
 		google.maps.event.addListener(marker, 'click', function() {
-		    // Handle click
+			// Handle click
 			console.log(magnitude);
 		});
 	}
@@ -107,38 +107,38 @@ var SeismoMap = function () {
 	}
 	
 	function hsv2rgb(h, s, v) {
-	  // adapted from http://schinckel.net/2012/01/10/hsv-to-rgb-in-javascript/
-	  var rgb, i, data = [];
-	  if (s === 0) {
-	    rgb = [v,v,v];
-	  } else {
-	    h = h / 60;
-	    i = Math.floor(h);
-	    data = [v*(1-s), v*(1-s*(h-i)), v*(1-s*(1-(h-i)))];
-	    switch(i) {
-	      case 0:
-	        rgb = [v, data[2], data[0]];
-	        break;
-	      case 1:
-	        rgb = [data[1], v, data[0]];
-	        break;
-	      case 2:
-	        rgb = [data[0], v, data[2]];
-	        break;
-	      case 3:
-	        rgb = [data[0], data[1], v];
-	        break;
-	      case 4:
-	        rgb = [data[2], data[0], v];
-	        break;
-	      default:
-	        rgb = [v, data[0], data[1]];
-	        break;
-	    }
-	  }
-	  return '#' + rgb.map(function(x){
-	    return ("0" + Math.round(x*255).toString(16)).slice(-2);
-	  }).join('');
+		// adapted from http://schinckel.net/2012/01/10/hsv-to-rgb-in-javascript/
+		var rgb, i, data = [];
+		if (s === 0) {
+			rgb = [v,v,v];
+		} else {
+			h = h / 60;
+			i = Math.floor(h);
+			data = [v*(1-s), v*(1-s*(h-i)), v*(1-s*(1-(h-i)))];
+			switch(i) {
+			case 0:
+				rgb = [v, data[2], data[0]];
+				break;
+			case 1:
+				rgb = [data[1], v, data[0]];
+				break;
+			case 2:
+				rgb = [data[0], v, data[2]];
+				break;
+			case 3:
+				rgb = [data[0], data[1], v];
+				break;
+			case 4:
+				rgb = [data[2], data[0], v];
+				break;
+			default:
+				rgb = [v, data[0], data[1]];
+				break;
+			}
+		}
+		return '#' + rgb.map(function(x){
+			return ("0" + Math.round(x*255).toString(16)).slice(-2);
+		}).join('');
 	}
 	
 	this.load = load;
